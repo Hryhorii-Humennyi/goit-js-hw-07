e444f8c8c668e500e465189cbfd86d915866395f
Hryhorii-Humennyi <80813832+Hryhorii-Humennyi@users.noreply.github.com>
2021-09-19 01:35:46 +0300

    task-1

---
+++ js/task-01.js
@@ -0,0 +1,4 @@
+const item = document.querySelectorAll('.item');
+console.log(`В списке ${item.length} категории.`);
+const categoriesText = document.getElementById("categories").querySelectorAll('h2');
+console.log('categoriesText', categoriesText);
