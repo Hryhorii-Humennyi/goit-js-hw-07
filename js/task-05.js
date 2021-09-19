fd5be8ff54c71e0da49a4bad6618f6ac1d282e48
Hryhorii-Humennyi <80813832+Hryhorii-Humennyi@users.noreply.github.com>
2021-09-19 23:16:57 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const visitor = document.querySelector('#name-input');
+
+const visitorName = document.querySelector('#name-output');
+
+visitor.addEventListener('input', () => {
+    visitorName.textContent = `${visitor.value}`;
+    if (visitor.value === '') {
+        visitorName.textContent = 'незнакомец';
+    }
+});
